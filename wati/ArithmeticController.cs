4e26c44713cca49c2ad9cd068f3e2d802c6a1053
Preeti Patel <preeti.patel@energyexemplar.com>
2023-09-15 09:35:03 +0530

    Arithmetic Api

---
+++ wati/ArithmeticController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wati
+{
+
+    public class ArithmeticController : Controller
+    {
+        [HttpPost("add")]
+        public async Task<int> AddAsync([FromQuery]int num1, [FromQuery] int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
